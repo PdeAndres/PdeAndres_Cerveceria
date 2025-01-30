fa81ecf7adcddc3a16f54ddf1e5031e76f6544de
PdeAndres <pablodeandresolano@gmail.com>
2025-01-30 18:31:56 +0100

    Login

---
+++ login.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+include('conexionBbdd.php');
+
+$correo = $_POST["usuario"];
+
+$password = $_POST["password"];
+
+
+
+$md5_password = md5($password);
+
+$query = "SELECT * FROM usuario WHERE correo = ? AND password = ?";
+
+$stmt = mysqli_prepare($conn, $query);
+
+if ($stmt) {
+    mysqli_stmt_bind_param($stmt, "ss", $correo, $md5_password);
+
+    mysqli_stmt_execute($stmt);
+
+    $resultado = mysqli_stmt_get_result($stmt);
+
+    if (mysqli_num_rows($resultado) == 1) {
+
+        $usuario = mysqli_fetch_assoc($resultado);
+
+        $_SESSION["usuario"] = $usuario;
+
+
+    } else {
+        session_destroy();
+        echo "No se ha encontrado ningun usuario";
+    }
+}
+?>
